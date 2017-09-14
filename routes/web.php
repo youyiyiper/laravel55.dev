f81be34585c4d728f223e1ff6faaa1282c73e824
xp <673726312@qq.com>
2017-09-14 21:58:26 +0800

    modify routes

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,60 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+//后台
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function(){
+    //登录页面 可切换
+    Route::get('login/{type?}', 'LoginController@index');
+    //登录提交
+    Route::post('login','LoginController@login');
+
+    //忘记密码 填写邮箱
+    Route::get('password/email','ForgotPasswordController@showLinkRequestForm');
+    //忘记密码 提交邮箱
+    Route::post('password/email', 'ForgotPasswordController@sendResetLinkEmail');
+
+    //打开邮箱负责地址跳转验证
+    Route::get('password/reset/{token?}','ResetPasswordController@showResetForm');
+    Route::post('password/reset','ResetPasswordController@reset');
+
+    //登录后 用中间件auth.admin 来验证登录状态
+    Route::group(['middleware' => 'auth.admin'], function () {
+        //首页
+        Route::get('/', 'IndexController@index');
+        Route::get('/index', 'IndexController@index');
+
+        //退出
+        Route::get('logout', 'LoginController@logout');
+
+        //用中间件rabc.check 来验证是否有权限操作
+        Route::group(['middleware' => 'rabc.check'], function () {
+
+            //修改密码
+            Route::get('manager/changePwd','ManagerController@changePwd');
+            Route::post('manager/postChangePwd','ManagerController@postChangePwd');
+
+            //信息中心
+            Route::get('manager/setting','ManagerController@setting');
+            Route::post('crop/upload','CropController@upload');
+            Route::post('crop/handle','CropController@handle');
+
+            //角色
+            Route::resource('role', 'rolesController');
+            //权限
+            Route::resource('privilege', 'privilegesController');
+            //菜单
+            Route::resource('sidebar', 'sidebarsController');
+            //后台用户
+            Route::resource('admin', 'adminsController');
+            Route::resource('category', 'categorysController');
+        });
+    });
 });
